635786447352cc184bc4828f9dabae7f7322a323
PmaiWoW <PmaiWoW@Gmail.com>
2019-06-14 16:43:12 +0100

    Add SuperList class, with various GetMinMax methods, using different multiple ways to return values

--- MultipleStuff/Program.cs
+++ MultipleStuff/Program.cs
@@ -6,7 +6,28 @@ namespace MultipleStuff
     {
         static void Main(string[] args)
         {
+            double min1, max1;
+            SuperList.MinMax2 MM2Struct;
+            Tuple<double, double> MM3Tuple;
+
             Console.WriteLine("Hello World!");
+            SuperList lst = new SuperList() { 12.3, 1.0, 0.4, 16, 92 };
+            
+            lst.GetMinMax1(out min1, out max1);
+
+            Console.WriteLine($"Min1: {min1}\nMax1: {max1}");
+
+            // 2nd GetMinMax
+            MM2Struct = lst.GetMinMax2();
+
+            Console.WriteLine($"Min2: {MM2Struct.Min}\n" +
+                $"Max2: {MM2Struct.Max}");
+
+            // 3rd GetMinMax
+            MM3Tuple = lst.GetMinMax3();
+
+            Console.WriteLine($"Min3: {MM3Tuple.Item1}\n" +
+                $"Max3: {MM3Tuple.Item2}");
         }
     }
 }
---
+++ MultipleStuff/SuperList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleStuff
+{
+    public class SuperList : List<double>
+    {
+        public void GetMinMax1(out double min, out double max)
+        {
+            // Maneira c# (programação por objetos)
+            Sort();
+            min = this[0];
+            max = this[Count - 1];
+
+            // Maneira C (programação procedimental)
+            // min = this[0];
+            // max = this[0];
+            // 
+            // for (int i = 1; i < Count; i++)
+            // {
+            //     if (this[i] < min) min = this[i];
+            //     else (this[i] > max) max = this[i];
+            // }
+        }
+
+        public struct MinMax2
+        {
+            public double Min { get; }
+            public double Max { get; }
+
+            public MinMax2(double min, double max)
+            {
+                Min = min;
+                Max = max;
+            }
+        }
+
+        public MinMax2 GetMinMax2()
+        {
+            double min, max;
+
+            GetMinMax1(out min, out max);
+
+            return new MinMax2(min, max);
+        }
+
+        public Tuple<double,double> GetMinMax3()
+        {
+            double min, max;
+
+            GetMinMax1(out min, out max);
+
+            return new Tuple<double, double>(min, max);
+        }
+    }
+}
